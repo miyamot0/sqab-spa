63d847081089fbf8348b1172e6b4ea44416893c1
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-08-30 10:43:11 -0500

    Update boilerplate for use with my domain

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: `${__dirname}/src/index.tsx`,
   output: {
     path: `${__dirname}/build`,
-    publicPath: '/build/',
+    publicPath: '/sqab-spa/build/',
     filename: 'bundle.js',
   },
 
@@ -45,7 +45,7 @@ module.exports = {
   // and test spa-github-pages redirect in dev
   devServer: {
     historyApiFallback: {
-      rewrites: [{ from: /\//, to: '/404.html' }],
+      rewrites: [{ from: /\/sqab-spa\/[^?]/, to: '/404.html' }],
     },
   },
 };
